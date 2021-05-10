ad3b9e7b4389ab2f9e888b9b534091e231f448d1
Tiago Ribeiro <1181444@isep.ipp.pt>
2021-05-10 13:10:11 +0100

    useEffect

--- Page-With-Login_Logout/src/components/Login/Login.js
+++ Page-With-Login_Logout/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,50 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // It executes for the first time the component was mounted, but never after
+  // useEffect(() => {
+  //   console.log('EFFECT RUNNING');
+  // }, []);
+
+  // Now this function re-runs whenever the component was re-evaluated AND the dependency 'enteredPassword' changed
+  // useEffect(() => {
+  //   console.log('EFFECT RUNNING');
+  // }, [enteredPassword]);
+
+  useEffect(() => {
+    console.log('EFFECT RUNNING');
+
+    return () => { //This will run BEFORE the effect function runs except for the first time it runs
+      console.log('EFFECT CLEANUP');
+    }
+  }, [enteredPassword]); //if here was an empty array instead [enteredPassword] as a dependency, the cleanup function would only run
+                         //when the component is removed from the DOM, for exemple, when I actually login
+
+  useEffect(() => {
+    const identifier =  setTimeout(() => {
+      console.log('checking validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 400);
+
+    //This runs as a cleanup process
+    //P: When it runs?
+    //R: Whenever the useEffect function runs, BEFORE it runs (except for the 1st time it runs), this cleanup function will run
+    //It also runs when the component is re-used (unmounts from the DOM)
+    //So basically it runs before every new side effect function execution & before the component is removed and it does NOT run before the side effect function execution
+    return () => {
+      console.log('cleanup');
+      clearTimeout(identifier); //everytime the cleanup functions runs, it clears the timer that was set in the last side effect function execution
+    };
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
