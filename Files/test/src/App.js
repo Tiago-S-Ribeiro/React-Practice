ad44fd57637a33671cab3df4b2548da91378e029
Tiago Ribeiro <1181444@isep.ipp.pt>
2021-05-21 19:55:57 +0100

    +++

--- Files/test/src/App.js
+++ Files/test/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FilterUnit from './components/FilterUnit/FilterUnit';
-// import Axis from './components/Axis/Axis';
+import Axis from './components/Axis/Axis';
 import logo from './assets/logo.png';
 import './App.css';
 
@@ -9,7 +9,8 @@ function App() {
     <div className="main-app">
       <h2>Abcde Dashboard</h2>
       <img src={logo} alt="this is a logo" width="200rem" height="50rem"></img>
-      <FilterUnit></FilterUnit>
+      <FilterUnit/>
+      <Axis/>
     </div>
   );
 }
---
+++ Files/test/src/components/Axis/Axis.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './Axis.css';
+
+const Axis = () => {
+    return(
+        <Card className="grid-block">
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+            <span>test card block i.</span>
+        </Card>
+    );
+}
+
+export default Axis;
